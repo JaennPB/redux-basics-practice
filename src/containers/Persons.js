8abe35ac5ec5d1d0efc1414e6b035be2fc13b286
jaennpb <jaenn.bucio@gmail.com>
2021-03-17 15:41:27 -0600

    finished

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Person from '../components/Person/Person';
-import AddPerson from '../components/AddPerson/AddPerson';
+import Person from "../components/Person/Person";
+import AddPerson from "../components/AddPerson/AddPerson";
+import * as actionTypes from "../store/actions";
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
-
-    render () {
-        return (
-            <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
-                    <Person 
-                        key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
-                ))}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <AddPerson personAdded={this.props.onPersonAdd} />
+        {this.props.personsArray.map((person) => (
+          <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            clicked={() => this.props.onPersonDelete(person.id)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Persons;
+const mapStateToProps = (state) => {
+  return {
+    personsArray: state.persons,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onPersonAdd: () => dispatch({ type: actionTypes.ADD_PERSON }),
+    onPersonDelete: (id) => dispatch({ type: actionTypes.DEL_PERSON, id: id }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,28 @@
+import * as actionTypes from "../actions";
+
+const initialState = {
+  persons: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_PERSON:
+      const newPerson = {
+        id: Math.random(),
+        name: "PERSON",
+        age: Math.floor(Math.random() * 40),
+      };
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson),
+      };
+    case actionTypes.DEL_PERSON:
+      return {
+        ...state,
+        persons: state.persons.filter((person) => person.id !== action.id),
+      };
+  }
+  return state;
+};
+
+export default rootReducer;
